5618531bf8b7bc10fd9c3c4082064e7a59ff3f83
Minh Hung <84724817+duongminhhung@users.noreply.github.com>
2022-11-21 11:12:57 +0700

    upload db

--- index.php
+++ index.php
@@ -1,4 +1,2 @@
 <?php 
     require 'router.php';
-
-
---
+++ view/home/test1.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+<style>
+.error {color: #FF0000;}
+</style>
+</head>
+<body>  
+
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format";
+    }
+  }
+    
+  
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  E-mail: <input type="text" name="email">
+  <span class="error">* <?php echo $emailErr;?></span>
+ 
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
+<?php
+echo $email;
+
+?>
+
+</body>
+</html>
